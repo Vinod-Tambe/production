49065e398d55d83fdf37e190a4b4a7fe3b33d12a
Vinod Gokul Tambe <109082@ondirect.local>
2025-06-23 21:06:08 +0530

    setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require(".");
+// const { connectDb } = require("./config/db");
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+    try {
+        // await connectDb();
+        console.log(`Ecommerce API listening on PORT ${PORT}`);
+    } catch (error) {
+        console.error("Database connection failed:", error);
+    }
+}).on('error', (err) => {
+    console.error(`Failed to start server on PORT ${PORT}:`, err.message);
+});
