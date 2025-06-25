eb8242a16090a163b8341cae7e1035e92d777717
Vinod Tambe <140325610+Vinod-Tambe@users.noreply.github.com>
2025-06-25 14:02:12 -0700

    updated code

--- routes/firm.route.js
+++ routes/firm.route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const firmController = require("../controllers/firm.controller");
 const ownerAuthenticate = require("../middleware/owner.authenticate");
-router.post("/",ownerAuthenticate, firmController.create_firm);
-router.put("/:id",ownerAuthenticate, firmController.update_firm);
+router.post("/create",ownerAuthenticate, firmController.create_firm);
+router.put("/update/:id",ownerAuthenticate, firmController.update_firm);
 router.get("/:id", ownerAuthenticate,firmController.get_firm_by_id);
-router.delete("/:id",ownerAuthenticate, firmController.delete_firm);
+router.delete("/delete/:id",ownerAuthenticate, firmController.delete_firm);
 router.get("/", ownerAuthenticate,firmController.get_all_firm);
 
 module.exports = router;
