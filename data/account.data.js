64a616fb32c10356a0e946ff5587e53566436952
Vinod Gokul Tambe <109082@ondirect.local>
2025-08-22 19:51:56 +0530

    update default account

--- data/account.data.js
+++ data/account.data.js
@@ -1,7 +1,7 @@
-const cr_acount_name = ["CASH IN HAND","ONLINE ACCOUNT"];
-const cr_acount_primary = ["CASH IN HAND","ONLINE ACCOUNT"];
+const cr_acount_name =    ["CAPITAL ACCOUNT"];
+const cr_acount_primary = ["CAPITAL ACCOUNT"];
 
-const dr_acount_name = ["BANK ACCOUNT"];
-const dr_acount_primary = ["BANK ACCOUNT"];
+const dr_acount_name =    ["BANK ACCOUNT","CARD ACCOUNT","ONLINE ACCOUNT","CASH IN HAND","UNSECURED LOANS"];
+const dr_acount_primary = ["BANK ACCOUNT","CARD ACCOUNT","ONLINE ACCOUNT","CASH IN HAND","UNSECURED LOANS"];
 
 module.exports = { cr_acount_name, cr_acount_primary, dr_acount_name, dr_acount_primary };
